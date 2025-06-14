d400bb918256710d17e27769981f96c636e12dac
mikosoup <roselabepimentel@gmail.com>
2025-06-14 21:35:10 +0800

    Policy Added

---
+++ app/Http/Policies/ReplyPolicy.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Reply;
+use App\Models\User;
+
+class ReplyPolicy
+{
+    public function update(User $user, Reply $reply)
+    {
+        return $user->id === $reply->user_id || $user->isAdmin();
+    }
+
+    public function delete(User $user, Reply $reply)
+    {
+        return $user->id === $reply->user_id || $user->isAdmin();
+    }
+}
